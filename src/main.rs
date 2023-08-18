214a60ff28619b56af67aeb41af12e378b1f3995
kibethh <kibethh@gmail.com>
2023-08-18 22:46:57 +0300

    To get rid of configuration.yaml

--- src/main.rs
+++ src/main.rs
@@ -13,11 +13,20 @@ async fn main() -> std::io::Result<()> {
     let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
     init_subscriber(subscriber);
     let configuration = get_configuration().expect("Failed to read configuration.");
+    let address = format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port
+    );
+    // let connection_pool =
+    //     PgPool::connect(&configuration.database.connection_string().expose_secret())
+    //         .await
+    //         .expect("Failed to connect to Postgres.");
+
+    // No longer async, given that we don't actually try to connect!
     let connection_pool =
-        PgPool::connect(&configuration.database.connection_string().expose_secret())
-            .await
-            .expect("Failed to connect to Postgres.");
-    let address = format!("127.0.0.1:{}", configuration.application_port);
+        PgPool::connect_lazy(&configuration.database.connection_string().expose_secret())
+            .expect("Failed to create Postgres connection pool.");
+    // let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
     run(listener, connection_pool)?.await
 }
